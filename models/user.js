6307944db2430254d24012d27f14c22885ed2afa
Ben <Darsabre@gmail.com>
2015-11-23 13:44:07 -0500

    salt and authentication for users

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 // Initialize from npm tools
 var mongoose   = require('mongoose')
 var SurveySchema = require('./survey.js').schema;
-
+var bcrypt = require ('bcrypt')
 // Define the User objects
 var UserSchema = new mongoose.Schema({
   name:     String,
@@ -15,6 +15,26 @@ var UserSchema = new mongoose.Schema({
         ref: 'Survey'
     }]
 });
+
+//when password is to be saved, first salt the password (if it has not already been salted)
+UserSchema.pre('save', function (next){
+  let user = this;
+  if (!user.ismodified('password')) return next();
+  bcrypt.genSalt(7, (err, salt) =>{
+    if (err) return next (err)
+    bcrypt.hash(user.password, salt, err, hash) => {
+      if (err) return next (err);
+      user.password=hash;
+      next()
+    })
+  })
+})
+// create functionality to compare the first password to the salted password, return bool.
+User.methods.authenticate = function(password, callback) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    callback(null, isMatch);
+  });
+};
 
 // Store model in var and export to the corresponding controller
 var User = mongoose.model('User', UserSchema)
