24c3b7802b3948e17dbfd135602a474b24cbcf48
Ben <Darsabre@gmail.com>
2015-11-23 12:18:32 -0500

    server changes to support tokens (incomplete)

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ let app = express();
 let bodyParser = require('body-parser');
 let logger = require('morgan');
 let mongoose = require('mongoose');
+let expressJWT = require('express-jwt');
+let jwt = require('jsonwebtoken');
 
 // Set Server Listener
 let server = app.listen(3000, function () {
   console.log("This app is running on Port 3000")
 })
-
 // Use these npm tools
-app.use(express.static('public'))
-app.use(bodyParser());
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}))
 app.use(logger('dev'))
+app.use(express.static(path.join(__dirname, 'public' )))
 
 // Connect to mongo database
 mongoose.connect('mongodb://localhost/fistOfFive', function (err) {
@@ -25,6 +29,9 @@ mongoose.connect('mongodb://localhost/fistOfFive', function (err) {
     console.log('connection successful');
   }
 });
+let db = mongoose.connection
+// make a secret.
+const secret = "keepitsecretkeepitsafe";
 
 // Controller Routes
 let userRoutes = require('./controllers/users_controller');
