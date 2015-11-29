446bb58637a28db8bea8b8e3c5f1ed996aa38f9a
Deven Davies <Deven_the_Impeccable@Devens-MacBook-Pro.local>
2015-11-29 15:45:16 -0500

    CSS and index.html orginization

--- public/app.js
+++ public/app.js
@@ -105,7 +105,7 @@ $(function(){
     $.ajax({
       url: 'surveys',
       type: 'POST',
-      data: surveyData,
+      data: surveyData
     }).done()
   };
 
@@ -116,15 +116,13 @@ $(function(){
 
     for(var i = 0; i < fistList.length; i++){
       if($('#finger' + i).data('clicked')){
-        //need to grab votes data from survey schema and increment it
-        //we may need to change the votes names to include numbers so we can iterate instad of writing new function for each
-        //deven, since you set up the survey model i thoguht you may have an idea for this
+        this.votes[i] += 1;
       }
     }
     $.ajax({
       url: '/surveys/:id',
       type: 'PUT',
-
+      data: surveyInc
     }).done()
   };
 
